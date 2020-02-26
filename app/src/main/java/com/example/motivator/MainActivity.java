ee18f6f77cccf280a3365ffc9700f4140f1836bf
IlmastMaksim <ilmastm9@gmail.com>
2020-02-26 11:29:53 +0200

    test change

--- app/src/main/java/com/example/motivator/MainActivity.java
+++ app/src/main/java/com/example/motivator/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String change;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
