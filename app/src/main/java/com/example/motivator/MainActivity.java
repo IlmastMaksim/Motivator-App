18559d0220121353579fab331d62073d8e3c8e36
IlmastMaksim <ilmastm9@gmail.com>
2020-02-26 13:30:50 +0200

    initial mainactivity added

--- app/src/main/java/com/example/motivator/MainActivity.java
+++ app/src/main/java/com/example/motivator/MainActivity.java
@@ -9,8 +9,6 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String change;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
